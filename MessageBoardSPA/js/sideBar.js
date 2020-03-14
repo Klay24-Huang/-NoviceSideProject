a23e7568d49206388559a05265a7ab884ad86c60
Vito <wkus963@gmail.com>
2020-03-14 21:56:38 +0800

    完成

--- MessageBoardSPA/js/sideBar.js
+++ MessageBoardSPA/js/sideBar.js
@@ -3,12 +3,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 /*關閉側欄，恢復原始側欄寬度，主體左跨度、背景透明度*/
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "#eef2ff";
 }
 
